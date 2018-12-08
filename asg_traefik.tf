71a8ce49af26fc232cc4280d49d8f8593ca0737c
Ezequiel Arielli <ezequiel.arielli@rappi.com>
2018-12-08 18:37:07 -0300

    separete autoscaling-groups to differents resources

--- asg_traefik.tf
+++ asg_traefik.tf
@@ -12,7 +12,7 @@ INTERNAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 sed -i s,MASTER_ENDPOINT,${aws_eks_cluster.eks-cluster.endpoint},g /var/lib/kubelet/kubeconfig
 sed -i s,CLUSTER_NAME,${var.cluster_defaults["name"]},g /var/lib/kubelet/kubeconfig
 sed -i s,REGION,${var.aws_region},g /etc/systemd/system/kubelet.service
-sed -i s,MAX_PODS,30,g /etc/systemd/system/kubelet.service
+sed -i s,MAX_PODS,20,g /etc/systemd/system/kubelet.service
 sed -i s,MASTER_ENDPOINT,${aws_eks_cluster.eks-cluster.endpoint},g /etc/systemd/system/kubelet.service
 sed -i s,INTERNAL_IP,$INTERNAL_IP,g /etc/systemd/system/kubelet.service
 DNS_CLUSTER_IP=10.100.0.10
@@ -26,16 +26,35 @@ systemctl restart kubelet kube-proxy
 USERDATA
 }
 
+###### ASG_TRAEFIK ######
+
+variable "group_traefik" {
+  description = "Default values for target groups as defined by the list of maps."
+  type        = "map"
+
+  default = {
+    name                 = "eks-nodes"    # Name for the eks workers.
+    ami_id               = "ami-dea4d5a1" # AMI ID for the eks workers. If none is provided, Terraform will searchfor the latest version of their EKS optimized worker AMI.
+    asg_desired_capacity = "2"            # Desired worker capacity in the autoscaling group.
+    asg_max_size         = "3"            # Maximum worker capacity in the autoscaling group.
+    asg_min_size         = "2"            # Minimum worker capacity in the autoscaling group.
+    instance_type        = "t2.small"     # Size of the workers instances.
+    key_name             = "virginia-kubernetes"      # The key name that should be used for the instances in the autoscaling group
+    ebs_optimized        = false          # sets whether to use ebs optimization on supported types.
+    public_ip            = false          # Associate a public ip address with a worker
+  }
+}
+
 resource "aws_launch_configuration" "traefik" {
   associate_public_ip_address = false
   iam_instance_profile        = "${aws_iam_instance_profile.eks-nodes.name}"
-  image_id                    = "${var.nodes_defaults["ami_id"]}"
-  instance_type               = "${var.nodes_defaults["instance_type"]}"
-  key_name                    = "${var.nodes_defaults["key_name"]}"
+  image_id                    = "${var.group_traefik["ami_id"]}"
+  instance_type               = "${var.group_traefik["instance_type"]}"
+  key_name                    = "${var.group_traefik["key_name"]}"
   name_prefix                 = "eks-traefik"
   security_groups             = ["${module.eks_nodes.id}"]
   user_data_base64            = "${base64encode(local.eks-traefik-userdata)}"
-  ebs_optimized               = "${var.nodes_defaults["ebs_optimized"]}"
+  ebs_optimized               = "${var.group_traefik["ebs_optimized"]}"
 
   lifecycle {
     create_before_destroy = true
@@ -47,10 +66,10 @@ resource "aws_launch_configuration" "traefik" {
 }
 
 resource "aws_autoscaling_group" "traefik" {
-  desired_capacity     = "${var.nodes_defaults["asg_desired_capacity"]}"
+  desired_capacity     = "${var.group_traefik["asg_desired_capacity"]}"
   launch_configuration = "${aws_launch_configuration.traefik.id}"
-  max_size             = "${var.nodes_defaults["asg_max_size"]}"
-  min_size             = "${var.nodes_defaults["asg_min_size"]}"
+  max_size             = "${var.group_traefik["asg_max_size"]}"
+  min_size             = "${var.group_traefik["asg_min_size"]}"
   name                 = "eks-traefik-asg"
 
   vpc_zone_identifier = [
